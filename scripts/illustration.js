48a188ebd8b1ee0a8622c0436b256ef944cc7da0
hugolhld <47746364+hugolhld@users.noreply.github.com>
2019-06-06 02:35:04 +0200

    rr

--- scripts/illustration.js
+++ scripts/illustration.js
@@ -17,7 +17,6 @@ class Slider {
       this.arrowNav
   
       this.init()
-  
       this.hideArrow()
       //this.filter()
     }
@@ -51,12 +50,10 @@ class Slider {
       this.arrowPrev = document.createElement('div')
       this.arrowPrev.classList.add('arrowPrevious')
       this.arrowNav.appendChild(this.arrowPrev)
-      this.arrowUP = document.createElement('div')
-      this.arrowUP.classList.add('up')
-      this.arrowDN = document.createElement('div')
-      this.arrowDN.classList.add('down')
-      this.arrowPrev.appendChild(this.arrowUP)
-      this.arrowPrev.appendChild(this.arrowDN)
+      this.arrowPrevImg = document.createElement('img')
+      this.arrowPrevImg.setAttribute('src', "images/left-arrow.svg")
+      this.arrowPrev.appendChild(this.arrowPrevImg)
+
   
       this.arrowNext.addEventListener('click',()=>
       {
@@ -106,6 +103,8 @@ class Slider {
       this.slider = document.querySelectorAll('.home__slider')
       this.sliderContent = document.querySelectorAll('.slider__selection')
       this.sliderImg = document.querySelectorAll('.slider__container')
+      
+      
   
       this.init()
     }
@@ -122,12 +121,26 @@ class Slider {
             {
               this.slider[j].style.display= 'block'
               this.sliderContent[j].classList.add('slider')              
+<<<<<<< HEAD
               new Slider(".slider", 1)
+=======
+              let mySlider = new Slider(".slider", 1)
+              this.divTemp = document.querySelector('.sliderContainer')
+              console.log(this.divTemp)
+              this.divTemp.classList.add('remove')
+>>>>>>> 12b4e9e243469c3def129e6873b0e329cea55ed9
             }
             else{
               this.sliderContent[j].classList.remove('slider')
               this.slider[j].style.display= 'none'
+<<<<<<< HEAD
               delete Slider(".slider",1)
+=======
+              this.divRemove = document.querySelector('.remove')
+              this.sliderContent[j].removeChild(this.divRemove)
+              
+              
+>>>>>>> 12b4e9e243469c3def129e6873b0e329cea55ed9
             }
           }
         })
