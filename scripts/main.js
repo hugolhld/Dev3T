55bcaf9749e263f3460b06b2642c51d60819e73e
hugolhld <47746364+hugolhld@users.noreply.github.com>
2019-06-12 02:53:49 +0200

    animation menu hamb

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,25 @@
+ /**
+             * forEach implementation for Objects/NodeLists/Arrays, automatic type loops and context options
+             *
+             * @private
+             * @author Todd Motto
+             * @link https://github.com/toddmotto/foreach
+             * @param {Array|Object|NodeList} collection - Collection of items to iterate, could be an Array, Object or NodeList
+             * @callback requestCallback      callback   - Callback function for each iteration.
+             * @param {Array|Object|NodeList} scope=null - Object/NodeList/Array that forEach is iterating over, to use as the this value when executing callback.
+             * @returns {}
+             */
+            var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+          
+            var hamburgers = document.querySelectorAll(".hamburger");
+            if (hamburgers.length > 0) {
+              forEach(hamburgers, function(hamburger) {
+                hamburger.addEventListener("click", function() {
+                  this.classList.toggle("is-active");
+                }, false);
+              });
+            }
+ 
  let i = 0,text
  text = 'Je m’appelle Cannelle Robert, j’ai 20 ans et suis actuellement étudiante en première année à l’école ESMA Nantes. Je suis dans la filière “cinéma d’animations 3D effets spéciaux” de cette école. Actuellement en première année, je réalise différentes créations en 2D et en 3D sous forme d’image. Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma, ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi, humanitatis multiformibus officiis retentabant. '
  let overlay = document.querySelector('.overlay')
